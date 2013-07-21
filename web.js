2c28c8eb19119771108cefb0fa7674803a3be9d2
athahar <athahar.md@gmail.com>
2013-07-21 03:05:39 +0000

    read static html from public dir

--- web.js
+++ web.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+var express = require('express'),
+    app = express();
 
+app.configure(function(){
+    app.use(express.logger('dev');
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
+})
+
+/*
 app.get('/', function(request, response) {
   response.send('Hello World! athar');
 });
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
