b38abfcb4a83474e6977a8f33d8989083e3302a6
athahar <athahar.md@gmail.com>
2013-07-21 03:12:08 +0000

    updated missing brackets

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function(){
-    app.use(express.logger('dev');
+    app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.static(__dirname + '/public'));
 });
