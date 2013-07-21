ba1b49631ba92af57ec85de9757f5e6a48f0e0cb
athahar <athahar.md@gmail.com>
2013-07-21 03:08:52 +0000

    require changes

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 var express = require('express'),
-    app = express();
+	fs = require('fs'),
+	sys = require('sys');
+ 
+var app = express();
 
 app.configure(function(){
     app.use(express.logger('dev');
     app.use(express.bodyParser());
     app.use(express.static(__dirname + '/public'));
-})
+});
 
 /*
 app.get('/', function(request, response) {
